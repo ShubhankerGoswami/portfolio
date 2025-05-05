dd22ad8ef709386b8487f627e4ae7f057fa64a09
ShubhankerGoswami <shubhaker55@gmail.com>
2025-05-05 15:25:11 +0530

    Initial commit for frontend

---
+++ portfolio.js
@@ -0,0 +1,15 @@
+const place = window.location.pathname;
+console.log(place);
+
+if (place === "/") {
+    document.querySelector(".navbar a[href='/']").style.color = "purple";
+    
+    }
+
+
+window.onload = function() {
+        setTimeout(function() {
+            document.querySelector('.transitionleft').classList.add('show');
+            document.querySelector('.transitionright').classList.add('show');
+        }, 100); // You can adjust the timeout delay as needed
+    };
